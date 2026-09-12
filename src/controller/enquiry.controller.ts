a801f3c5af03372c053aaeabfbabc11af83f2be9
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-09-12 22:18:51 +0545

    feat: refactor enquiry controller to use asyncHandler and improve error handling

--- src/controller/enquiry.controller.ts
+++ src/controller/enquiry.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { Enquiry as EnquiryService} from "../service/enquiry.service"; 
 import { StatusCode } from "../constant/statusCode.interface";
+import { Message } from "../constant/message.interface";
+import { expressError } from "../utils/expressError";
+import { asyncHandler } from "../utils/asyncHandler";
 
 export class Enquiry {
   private enquiryService: EnquiryService;
@@ -9,31 +12,51 @@ export class Enquiry {
     this.enquiryService = new EnquiryService();
   }
 
-  createEnquiry = async (req: Request, res: Response) => {
-    const enquiry = await this.enquiryService.createEnquiry(req.body);
+  createEnquiry = asyncHandler(async (req: Request, res: Response) => {
+    const {name, email, date, guests, message} = req.body;
+    if(!name || !email || !date || !guests){
+      throw new expressError(StatusCode.BAD_REQUEST, Message.BAD_REQUEST)
+    }
+
+    await this.enquiryService.createEnquiry({name, email, date, guests, message});
 
     return res.status(StatusCode.CREATED).json({
       success: true,
-      message: "Enquiry submitted successfully",
-      data: enquiry
+      message: "Enquiry submitted successfully"
     });
-  };
+  });
 
-  getAllEnquiries = async (req: Request, res: Response) => {
+  getAllEnquiries = asyncHandler(async (req: Request, res: Response) => {
     const enquiries = await this.enquiryService.getAllEnquiries();
 
     return res.status(StatusCode.OK).json({
       success: true,
       data: enquiries,
     });
-  };
+  });
 
-  getEnquiryById = async (req: Request, res: Response) => {
-    const enquiry = await this.enquiryService.getEnquiryById(String(req.params.id));
+  getEnquiryById = asyncHandler(async (req: Request, res: Response) => {
+    const id = String(req.params.id);
+    if (!id) {
+      throw new expressError(StatusCode.BAD_REQUEST, "Enquiry ID is required");
+    }
+    const enquiry = await this.enquiryService.getEnquiryById(id);
 
     return res.status(StatusCode.OK).json({
       success: true,
       data: enquiry,
     });
-  };
+  });
+  deleteEnquiry = asyncHandler(async (req: Request, res: Response) => {
+    const id = String(req.params.id);
+    if (!id) {
+      throw new expressError(StatusCode.BAD_REQUEST, "Enquiry ID is required");
+    }
+    await this.enquiryService.deleteEnquiry(id);
+
+    return res.status(StatusCode.OK).json({
+      success: true,
+      message: "Enquiry deleted successfully",
+    });
+  });
 }
