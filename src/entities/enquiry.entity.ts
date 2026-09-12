2f9ebf8632b3fa8d655e702f1ccdf3849b66848d
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-09-12 22:23:37 +0545

    feat: enforce non-nullable constraints on Enquiry fields

--- src/entities/enquiry.entity.ts
+++ src/entities/enquiry.entity.ts
@@ -4,20 +4,18 @@ import { Base } from "./base.entity";
 
 @Entity("enquiries")
 export class Enquiry extends Base {
-  @Column()
+  @Column({nullable:false})
   name: string;
 
-  @Column()
+  @Column({nullable: false})
   email: string;
 
-  @Column({ type: "date" })
+  @Column({ type: "date" , nullable:false})
   date: Date;
 
-  @Column()
+  @Column({nullable: false})
   guests: number;
 
   @Column({ type: "text", nullable: true })
   message: string;
-
- 
 }
